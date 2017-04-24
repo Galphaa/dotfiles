3be8046e88859bb3dc31d147bd2e38ac13b8bb21
Galphaa <gkeratishvili@gmail.com>
2017-04-24 15:20:10 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,14 +2,23 @@
 
 apt-get -y update && apt-get -y upgrade 
 
-cat list.txt | xargs apt-get -y install 
+cat list.txt | xargs apt-get -y install   # Installing all files needed for first usage
 
 apt-get -y update && apt-get -y upgrade 
 
-wget https://atom.io/download/deb
+cd
+mkdir .installs
+cd .installs
+wget https://atom.io/download/deb    # Installing ATOM
 dpkg -i deb
 apt-get -y update
 
-add-apt-repository -y ppa:nilarimogard/webupd8
+wget http://archive.getdeb.net/ubuntu/pool/apps/s/synergy/synergy_1.8.8-1~getdeb1_amd64.deb   # Installing synergy 1.8.8 
+dpkg -i synergy_1.8.8-1~getdeb1_amd64.deb
+apt-get -f install 
+
+add-apt-repository -y ppa:nilarimogard/webupd8   # installing repo and then installing pulseaudio-equalizer for more easy control for audio system
 apt-get update
 apt-get install pulseaudio-equalizer
+
+
